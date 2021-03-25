cfe986a5538afe60c6fd6e39b1f9f52e12eb7d9e
abmaschietto <arturboudoux@gmail.com>
2021-03-25 08:49:49 -0300

    Og tag added, proper router handling to unknow urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ import { SkillpageComponent } from './main/skillpage/skillpage.component';
 
 
 const routes: Routes = [
-   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path:'**', component:MainComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home', component:MainComponent, data: {animation: 'Home'}},
   {path:'email', component:EmailpageComponent,  data: {animation: 'Contato'}},
   {path:'skills', component:SkillpageComponent,  data: {animation: 'Skills'}},
-  {path:'backstage', component:BackstageComponent,  data: {animation: 'Backstage'}},
-  {path:'', component:MainComponent},
+  {path:'backstage', component:BackstageComponent,  data: {animation: 'Backstage'}}
 ];
 
 @NgModule({
